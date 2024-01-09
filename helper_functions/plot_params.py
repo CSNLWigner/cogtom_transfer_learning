fd56271d3ab6d0e53b78c18df761f706c737b01e
SzekelyAnna <54469292+SzekelyAnna@users.noreply.github.com>
2024-01-09 17:04:57 +0100

    Add files via upload

---
+++ helper_functions/plot_params.py
@@ -0,0 +1,23 @@
+
+
+class PlotParams: 
+    figsize = (6.4, 4.8)
+    pairfigsize = (10.4, 4.8)
+    tripl150igsize = (10.4, 4.8)
+
+    ## Fontsitzes
+    titlefontsize = 28
+    axlabsize = 28
+    ticklabsize = 20
+    legendfontsize = 20
+    starsize = 24
+
+    ## Marker sizes
+    markersize_seaborn = 100
+    markersize_matplotlib = 8
+
+    ## Colors
+    basic_color = '#0653C7'
+    markov_blue = '#5576D1'
+    ct_orange = '#FFC600'
+    alpha = 0.6
